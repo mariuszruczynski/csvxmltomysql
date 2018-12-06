7cf902875f9604f7339c7141e0ee9eb4987eba82
Mariusz <you@example.com>
2018-12-06 19:27:08 +0100

    add Csv to sql class and static metod read file

--- src/main/java/csvxmltomysql/CsvToSql.java
+++ src/main/java/csvxmltomysql/CsvToSql.java
@@ -1,6 +1,34 @@
 package csvxmltomysql;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CsvToSql {
+
+     public static void readCsvFile(String fileName) {
+
+        Charset charset = Charset.forName("utf-8");
+        Path path = Paths.get(fileName);
+        String line;
+
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)){
+
+            do {
+                line = reader.readLine();
+                if (line != null) {
+                    System.out.println(line);
+                }
+            } while (line != null);
+        } catch (IOException ex) {
+            System.out.println("The file could not be loaded: " + fileName);
+        }
+
+    }
+
 
 
 
--- src/main/java/csvxmltomysql/Main.java
+++ src/main/java/csvxmltomysql/Main.java
@@ -1,33 +1,13 @@
 package csvxmltomysql;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class Main {
 
     public static void main(String... args) {
 
-        String filename = "dane-osoby.csv";
-        Charset charset = Charset.forName("utf-8");
-        Path path = Paths.get(filename);
-        String line;
+        String fileName = "dane-osoby.csv";
 
-        try (BufferedReader reader = Files.newBufferedReader(path, charset)){
 
-            do {
-                line = reader.readLine();
-                if (line != null) {
-                    System.out.println(line);
-                }
-            } while (line != null);
-        } catch (IOException ex) {
-            System.out.println("The file could not be loaded: " + filename);
-        }
-
+        CsvToSql.readCsvFile(fileName);
     }
 }
 
